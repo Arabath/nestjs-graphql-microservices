44430649f141f376bf72331c6f2f2e6063c86fab
Arabath <axelhobrecht@hotmail.com>
2024-10-24 12:23:56 -0300

    dependencies installed, basic config added

--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  create(createUserInput: CreateUserInput) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
   }
 }
